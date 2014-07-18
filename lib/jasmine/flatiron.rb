9c59ea1479ce93b5146d808dc1143149f06a1d26
Logan Hasson <logan.hasson@gmail.com>
2014-07-18 12:03:16 -0400

    Working on porting over javascript

--- lib/jasmine/flatiron.rb
+++ lib/jasmine/flatiron.rb
@@ -2,6 +2,33 @@ require "jasmine/flatiron/version"
 
 module Jasmine
   module Flatiron
-    # Your code goes here...
+    class PhantomChecker
+      def self.installed?
+        new.installed?
+      end
+
+      def installed?
+        if !brew_installed?
+          puts "You must have Homebrew installed."
+          exit
+        else
+          if !phantom_installed?
+            install_phantomjs
+          end
+        end
+      end
+
+      def brew_installed?
+        !`which brew`.empty?
+      end
+
+      def phantom_installed?
+        !`brew ls --versions phantomjs`.empty?
+      end
+
+      def install_phantomjs
+        `brew install phantomjs`
+      end
+    end
   end
 end
