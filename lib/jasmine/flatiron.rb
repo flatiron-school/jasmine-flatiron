110dccb59e79fd31aeb46fb43e5a5f0cefc8b725
Logan Hasson <logan.hasson@gmail.com>
2014-07-21 14:44:59 -0400

    Format JSON better and add username and repo

--- lib/jasmine/flatiron.rb
+++ lib/jasmine/flatiron.rb
@@ -2,6 +2,40 @@ require "jasmine/flatiron/version"
 
 module Jasmine
   module Flatiron
+    class UsernameParser
+      def self.get_username
+        netrc = Netrc.read
+        username, token = netrc["flatiron-push"]
+        if !username
+          print "Enter your github username: "
+          username = gets.strip
+          netrc["flatiron-push"] = username, "none"
+          netrc.save
+        end
+
+        username
+      end
+    end
+
+    class RepoParser
+      def self.get_repo
+        repo = Git.open(FileUtils.pwd)
+
+        begin
+          url = repo.remote.url
+        rescue
+          puts "Not a valid Git repository"
+          die
+        end
+
+        repo_name = url.match(/(?:https:\/\/|git@).*\/(.+)\.git/)[1]
+      end
+
+      def self.die
+        exit
+      end
+    end
+
     class PhantomChecker
       def self.check_installation
         new.check_installation
@@ -76,17 +110,19 @@ module Jasmine
       attr_reader :no_color, :local, :browser, :conn, :color_opt
       attr_accessor :json_results
 
-      def self.run(options)
-        new(options).run
+      def self.run(username, repo, options)
+        new(username, repo, options).run
       end
 
-      def initialize(options)
+      def initialize(username, repo, options)
         @no_color = !!options[:color]
         @color_opt = !no_color ? "" : "NoColor"
         @local = !!options[:local]
         @browser = !!options[:browser]
         @json_results = {
-          testsuites: [],
+          username: username,
+          repo: repo,
+          examples: [],
           tests: 0,
           errors: 0,
           failures: 0,
@@ -105,22 +141,21 @@ module Jasmine
           system("open #{FileFinder.location_to_dir('runners')}/SpecRunner#{color_opt}.html")
         else
           system("phantomjs #{FileFinder.location_to_dir('runners')}/run-jasmine.js #{FileFinder.location_to_dir('runners')}/SpecRunner#{color_opt}.html")
+          make_json
         end
 
-        make_json
-        puts json_results.inspect
-        # unless local
-        #   push_to_flatiron
-        # end
+        if !local && !browser
+          push_to_flatiron
+        end
 
-        # clean_up
+        clean_up
       end
 
       def make_json
-        Dir.entries(FileUtils.pwd).keep_if { |f| f.match(/TEST/) }.each do |f|
+        test_xml_files.each do |f|
           parsed = JSON.parse(Crack::XML.parse(File.read(f)).to_json)["testsuites"]["testsuite"]
-          json_results[:testsuites] << parsed["testcase"]
-          json_results[:testsuites].flatten!
+          json_results[:examples] << parsed["testcase"]
+          json_results[:examples].flatten!
           json_results[:tests] += parsed["tests"].to_i
           json_results[:errors] += parsed["errors"].to_i
           json_results[:failures] += parsed["failures"].to_i
@@ -129,7 +164,7 @@ module Jasmine
       end
 
       def push_to_flatiron
-        conn.post(SERVICE_ENDPOINT, )
+        conn.post(SERVICE_ENDPOINT, json_results)
       end
 
       def make_runner_html
@@ -148,8 +183,14 @@ module Jasmine
         end
       end
 
+      def test_xml_files
+        Dir.entries(FileUtils.pwd).keep_if { |f| f.match(/TEST/) }
+      end
+
       def clean_up
-        FileUtils.rm("#{FileFinder.location_to_dir('runners')}/SpecRunner#{color_opt}.html")
+        test_xml_files.each do |file|
+          FileUtils.rm(file)
+        end
       end
     end
   end
